de7a3a07472c3bdea8bd58041dd5d9470d1d6c26
Sean Eastin <s198013@students.aie.edu>
2019-12-18 11:26:37 -0600

    finishied part 1 of assessment

--- Zorp/Zorp.cpp
+++ Zorp/Zorp.cpp
@@ -6,7 +6,61 @@
 
 int main()
 {
-    std::cout << "Hello World!\n"; 
+	int height = 0;
+	char firstLetterOfName = 0;
+	int avatarHP = 0;
+	std::cout << "Welcome to ZORP!" << std::endl;
+	std::cout << "ZORP is a game of adventure, danger, and low cunning." << std::endl;
+	std::cout << "it is definitely not related to any other text-based adventure game." << std::endl << std::endl;
+	std::cout << "First, some questions..." << std::endl;
+	std::cout << "How tall are you, in centimeters?" << std::endl;
+	std::cin >> height;
+	if (std::cin.fail())
+	{
+		std::cout << "You have failed the first challenge are are eaten by a grue." << std::endl;
+	}
+	else
+	{
+		std::cout << "You entered " << height << std::endl;
+	}
+
+	std::cin.clear();
+	std::cin.ignore(std::cin.rdbuf()->in_avail());
+
+
+	std::cout << "What is the first letter of your name? " << std::endl;
+
+	std::cin >> firstLetterOfName;
+
+	if (std::cin.fail() || !isalpha(firstLetterOfName))
+	{
+		std::cout << "You have failed the second challenge and are eaten by a grue." << std::endl;
+	}
+	else
+	{
+		std::cout << "You entered " << firstLetterOfName << std::endl;
+	}
+
+	std::cin.clear();
+	std::cin.ignore(std::cin.rdbuf()->in_avail());
+
+	if (firstLetterOfName != 0)
+	{
+		avatarHP = (float)height / (firstLetterOfName * 0.02f);
+	}
+	else
+	{
+		avatarHP = 0;
+	}
+	
+
+	std::cout << std::endl << "Using a complex deterministic algorithm, it has been calculated that you have " << avatarHP << " hit point(s)." << std::endl;
+
+
+	std::cout << std::endl << "Press 'Enter' to exit the program." << std::endl;
+	std::cin.get();
+	return 1;
+
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
